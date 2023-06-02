3dcfc32e0a59eff5ab9c901577a3258f19f1d0ad
keyuhan <hankeyu2021@gmail.com>
2023-06-02 13:42:18 -0500

    add docker, kubernetes, minikute prac

---
+++ 20230602kubenetes/spring-docker-sample/src/test/java/com/microprac/springdockersample/SpringDockerSampleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.microprac.springdockersample;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringDockerSampleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
